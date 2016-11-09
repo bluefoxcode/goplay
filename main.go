cdc271fcce810aaa6aa6daedcbbfa500f311b860
bluefoxcode <aaron@pop42.com>
2016-11-08 23:17:52 -0700

    added simple webserver

--- main.go
+++ main.go
@@ -2,7 +2,10 @@ package main
 
 import (
 	"log"
+	"net/http"
 	"os"
+
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
@@ -11,4 +14,18 @@ func main() {
 	if port == "" {
 		log.Fatal("$PORT must be set")
 	}
+
+	// should be handled in its own routes world, but for now this will work as I learn.
+
+	router := gin.New()
+	router.Use(gin.Logger())
+	router.LoadHTMLGlob("templates/*.tmpl.html")
+	router.Static("/static", "static")
+
+	router.GET("/", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "index.tmpl.html", nil)
+	})
+
+	router.Run(":" + port)
+
 }
