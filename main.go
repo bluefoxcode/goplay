59ae8136c1e29fe9bbecdef9bd472252c5bea2b4
bluefoxcode <aaron@pop42.com>
2016-11-09 01:00:47 -0700

    added fontawesome

--- main.go
+++ main.go
@@ -17,13 +17,13 @@ func main() {
 
 	// should be handled in its own routes world, but for now this will work as I learn.
 
-	router := gin.New()
+	router := gin.Default()
 	router.Use(gin.Logger())
-	router.LoadHTMLGlob("templates/*.tmpl.html")
+	router.LoadHTMLGlob("templates/**/*")
 	router.Static("/static", "static")
 
 	router.GET("/", func(c *gin.Context) {
-		c.HTML(http.StatusOK, "index.tmpl.html", nil)
+		c.HTML(http.StatusOK, "pages/index.tmpl.html", nil)
 	})
 
 	router.Run(":" + port)
